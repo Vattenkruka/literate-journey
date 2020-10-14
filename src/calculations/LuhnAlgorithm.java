af05e89c63bdb39f518db1c377b0462fdc580bc5
Oscar Dahlquist <Oscar.Dahlquist@gmail.com>
2020-10-14 14:59:08 +0200

    Vattenkruka - Fix: Refactoring of main and LuhnAlgorithm

--- src/calculations/LuhnAlgorithm.java
+++ src/calculations/LuhnAlgorithm.java
@@ -11,15 +11,13 @@ public class LuhnAlgorithm {
     public void userInput() {
         Scanner input = new Scanner(System.in);
         try {
-            System.out.println("Please enter your credit card number: ");
             userCardNo = input.nextLine();
-
+            getLastDigitOfCardNo();
         } catch (InputMismatchException ime) {
             System.out.println(ime.getMessage());
         } catch (Exception e) {
             System.out.println(e.getMessage());
-        }
-        finally{
+        } finally {
             input.close();
         }
 
@@ -29,29 +27,45 @@ public class LuhnAlgorithm {
         return userCardNo.length();
     }
 
-    public static boolean luhnCalculator(){
+    public static boolean luhnCalculator() {
         int sumOfDigits = 0;
         boolean everyOtherDigit = false;
-        for (int i = userCardNo.length() - 1; i >= 0; i--){
-                int digit = Integer.parseInt(userCardNo.substring(i, i+1));
+        for (int i = userCardNo.length() - 1; i >= 0; i--) {
+            int digit = Integer.parseInt(userCardNo.substring(i, i + 1));
 
-                if (everyOtherDigit) {
-                     digit *=2;
-                     if (digit > 9){
-                         int sum = 0;
-                         sum += digit/10;
-                         sum += digit % 10;
+            if (everyOtherDigit) {
+                digit *= 2;
+                if (digit > 9) {
+                    int sum = 0;
+                    sum += digit / 10;
+                    sum += digit % 10;
 
-                     }
-                 }
-                sumOfDigits += digit;
-                everyOtherDigit = !everyOtherDigit;
+                }
+            }
+            sumOfDigits += digit;
+            everyOtherDigit = !everyOtherDigit;
         }
         System.out.println(sumOfDigits);
         return (sumOfDigits % 10 == 0);
-
-
     }
 
+    public int getLastDigitOfCardNo(){
+        int lastDigit = Integer.parseInt(userCardNo.substring(userCardNo.length()-1));
+        return lastDigit;
+    }
+
+    public String checkSumValidation(){
+        if(luhnCalculator()){
+            return "Valid";
+        }
+        return "Not valid";
+    }
+
+    public String checkLengthOfUserInput(){
+        if(lengthOfUserInput() == 16){
+            return "(Credit card)";
+        }
+        return "(Not valid format for credit card)";
+    }
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -6,10 +6,16 @@ public class Main {
 
     public static void main(String[] args) {
         LuhnAlgorithm luhnAlgorithm = new LuhnAlgorithm();
+        System.out.println("Please enter your credit card number: ");
         luhnAlgorithm.userInput();
-        luhnAlgorithm.lengthOfUserInput();
-        luhnAlgorithm.luhnCalculator();
+        System.out.println("Provided: " + luhnAlgorithm.getLastDigitOfCardNo());
+        System.out.println("Expected: ");
+        System.out.println(" ");
+
+        System.out.println("Checksum: " + luhnAlgorithm.checkSumValidation());
+        System.out.println("Digits: " + luhnAlgorithm.lengthOfUserInput() + " " + luhnAlgorithm.checkLengthOfUserInput());
 
 	// write your code here
     }
+
 }
